11ca356bd0ddd50a62a56ef531ccd1acaec518cf
Dung Nguyen <contact@dungnt.net>
2023-05-31 21:12:58 +0700

    feat: init project support instagram

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nhymxu - Social ghost 👀
+
+A browser extension that blocks "seen" marks on Facebook stories and Instagram stories, because **privacy**.
+
+## Support
+
+Works for Chrome, Firefox, Opera, and any browser that supports [WebExtensions](https://developer.mozilla.org/en-US/Add-ons/WebExtensions).
+
+## Manual installation
+
+For **manual installation**, download the [latest release](https://github.com/nhymxu/social-ghost-ext/releases), unpack it, and load manually as an unpacked extension in your browser. (See tutorial: [Loading an unpacked extension](https://dev.opera.com/extensions/testing/#loading-an-unpacked-extension))
---
+++ index.js
@@ -0,0 +1,39 @@
+const URLS_TO_CANCEL = [
+    '*://*.instagram.com/api/graphql'
+]
+
+const isNodeEnv = typeof exports !== 'undefined'
+
+// Chrome support: `browser` should fallback to `chrome`
+// since Chrome doesn't fully support WebExtensions
+if (typeof browser === 'undefined' && !isNodeEnv) {
+    browser = chrome
+}
+
+const handleRequest = (details) => {
+    if(details.method == "POST") {
+        let formData = details.requestBody.formData;
+        let cancel = false;
+
+        if(formData) {
+            if (
+                formData.hasOwnProperty("fb_api_req_friendly_name") 
+                && formData.fb_api_req_friendly_name.includes("PolarisAPIReelSeenMutation")
+            ) {
+                cancel = true;
+            }
+        }
+
+        return {cancel: cancel};
+    }
+}
+
+const listenToRequests = () => {
+    browser.webRequest.onBeforeRequest.addListener(
+        handleRequest,
+        { urls: URLS_TO_CANCEL },
+        ['blocking', 'requestBody']
+    );
+}
+
+listenToRequests();
